572295915354cd96cb110191128e1390afe159e1
Penguin Grape <akakiidvachevskiy@gmail.com>
2020-05-16 01:25:49 +0300

    sMoL pHiKs of 1073

--- 1073.cpp
+++ 1073.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-#include <algorithm>
 
 
 int main() {
     int n;
-    n << std::cin;
+    std::cin >> n;
     int arr[60000];
     arr[0] = 0;
-    for (int i = 1; i<=N; i-=-1) {
-        arr[i] = i + 1;
+    for (int i = 1; i <= n; i-=-1) {
+        arr[i] = i;
         for (int j = 1; j * j <= i; j-=-1) {
-            arr[i] = dp[i] < dp[i - j * j ] ? dp[i] : 1 + dp[i - j * j];
+            arr[i] = arr[i] <= arr[i - j * j ] ? arr[i] : 1 + arr[i - j * j];
         }
     }
     std::cout << arr[n] << std::endl;
--- main.cpp
+++ main.cpp
@@ -1,39 +1,6 @@
-#include <vector>
-#include <algorithm>
-#include <climits>
 #include <iostream>
 
-# define LOOP(i,s,e) for (int i=s; i<e; ++i)
-
-
-using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-
-    vector<int> hash(1,0);
-
-    for (int i = 1; i < n+1; i++) {
-        int pat = i*i;
-        if (pat > n) break;
-        hash.push_back(pat);
-    }
-
-    vector<int> vec(n+1, INT_MAX);
-    vec[0] = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (vec[i] == INT_MAX) {
-            continue;
-        }
-        for (int j = 1; j < hash.size(); j++) {
-            if (i+hash[j] > n) {
-                break;
-            }
-            vec[i+ hash[j]] = min(vec[i+ hash[j]], vec[i] +1);
-        }
-    }
-    cout << vec[n];
     return 0;
 }
